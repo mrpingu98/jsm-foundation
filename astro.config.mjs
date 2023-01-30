8f328bd6c425eb2290ec12ccbafd6d8063d22592
mrpingu98 <salh98manvir@gmail.com>
2023-01-30 20:50:58 +0000

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mrpingu98.github.io'
+    site: 'https://mrpingu98.github.io',
+    base: '/my-repo',
 });
